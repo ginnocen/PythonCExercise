dd9aceee849a7917e9cf8af9c4f44ac9e759e301
gian michele innocenti <ginnocen@cern.ch>
2017-08-12 16:31:44 -0400

    update power law

--- PowerLaw.C
+++ PowerLaw.C
@@ -1,13 +1,8 @@
-int PowerLaw(){
+void PowerLaw(int base=5, int expo=2){
 
-
-
-
-
-
-
-
-
-
-
+  int result=1;
+  for (int i=1;i<=expo;i++){
+    result=result*base;
+  }
+  cout<<"result="<<result<<endl;
 }
